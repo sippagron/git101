619db86203140158adfd7809f87ab483cd2d4338
sippagron <sippagron.v@ku.th>
2019-10-28 15:05:55 +0700

    Add lab2

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Hero
+
+int main() {
+    int levell,level;
+    string namel,name;
+    cin >> namel;
+    cin >> levell;
+    Hero H(namel,levell);
+    cout << H.getName() << " " << H.getLevel();
+}
